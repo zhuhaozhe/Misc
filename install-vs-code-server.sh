249eb1528c13f025fe5323c1b7a445ef42b963a4
zhuhaozhe <haozhe.zhu@intel.com>
2023-12-18 13:17:19 +0800

    Create install-vs-code-server.sh

---
+++ install-vs-code-server.sh
@@ -0,0 +1,6 @@
+cd ~/.vscode-server/bin
+rm -rf ./*
+wget https://update.code.visualstudio.com/commit:${commit_id}/server-linux-x64/stable
+mv stable stable.tar.gz
+tar -zxvf stable.tar.gz
+mv vscode-server-linux-x64 ${commit_id}
