56ff337485cdfb0897f0309a121855538a26c573
zhuhaozhe <haozhe.zhu@intel.com>
2024-03-26 13:09:18 +0800

    Update install-vs-code-server.sh

--- install-vs-code-server.sh
+++ install-vs-code-server.sh
@@ -1,5 +1,5 @@
 cd ~/.vscode-server/bin
-rm -rf ./${commit_id}/*
+rm -rf ./*
 wget https://update.code.visualstudio.com/commit:${commit_id}/server-linux-x64/stable
 mv stable stable.tar.gz
 tar -zxvf stable.tar.gz
